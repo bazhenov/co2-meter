9219e2297127e7a8f3982c801a30715920d80e13
Denis Bazhenov <bazhenov@farpost.com>
2021-01-04 23:31:55 +1000

    Some lints are fixed

--- prometheus-exporter/src/main.rs
+++ prometheus-exporter/src/main.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::io;
 use std::time::Duration;
 
-use std::io::prelude::*;
 use serial::prelude::*;
 use std::convert::TryInto;
 
@@ -32,9 +31,9 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
         stop_bits: serial::Stop1,
         flow_control: serial::FlowNone
     };
-    port.configure(&settings);
+    port.configure(&settings)?;
 
-    port.set_timeout(Duration::from_millis(5000));
+    port.set_timeout(Duration::from_millis(5000))?;
 
     let mut buf: Vec<u8> = vec![0; 4];
 
@@ -43,7 +42,7 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
         let voltage: f32 = (f32::from(value) / 1024f32) * 5f32;
         let co2 = (((voltage - 1f32) / 4f32) * 2000f32) as i64;
         println!("({}), {}", value, co2);
-        let guard = exporter.wait_duration(std::time::Duration::from_millis(100));
+        let _guard = exporter.wait_duration(Duration::from_millis(100));
         gauge.with_label_values(&["1"]).set(co2);
     }
 
